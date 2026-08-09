fa69ee9ac2988d37391ddcca036203f07f7e5a53
vashdvorik <kinokaef@gmail.com>
2026-08-09 19:36:44 +0300

    2  Debug install

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Keep indexed string columns compatible with older MySQL/MariaDB servers.
+        Schema::defaultStringLength(191);
     }
 }
